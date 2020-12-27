a85bac45b466168e8980648deb4e94e201c0c6b9
Chlorine Pentoxide <69286505+ChlorinePentoxide@users.noreply.github.com>
2020-12-27 19:29:37 +0530

    Update winmake.cmd

--- src/qhs/winmake.cmd
+++ src/qhs/winmake.cmd
@@ -6,4 +6,5 @@ echo "Main-Class: Main" > META-INF/MANIFEST.MF
 jar -cf "%QUICK_OUT%\qhs.jar" META-INF/MANIFEST.MF *.class
 del /F/Q/S META-INF > nul
 rmdir /Q/S META-INF
+del /F/Q/S *.jar > nul
 del /F/Q/S *.class > nul
